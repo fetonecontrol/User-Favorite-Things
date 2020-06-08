fd58e309b811dc34feb7a6ef39b731354c298afc
Ian Gregg <iangregg188@gmail.com>
2020-06-08 11:53:46 -0700

    debugged the JS code to show a new array with the pushed user imputs
    Co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   $("form").submit(function(event) {
-    event.preventDefault ();
+    event.preventDefault();
 
     let arr1 = [$("#Fav1").val(), $("#Fav2").val(), $("#Fav3").val()];
     let arr2 = [];
     arr2.push(arr1[1], arr1[0], arr1[2]);
-    console.log()
-    $(".user").text(arr2);
-
+    console.log(arr2);
+    $("#user0").text(arr2[0]);
+    $("#user1").text(arr2[1]);
+    $("#user2").text(arr2[2]);
+  });
 });
