02f94f46067c12394962b3fe92d7cef5c666e088
Ian Gregg <iangregg188@gmail.com>
2020-06-08 11:11:31 -0700

    Established webpage for user favorite things.
    Co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+  $("form").submit(function(event) {
+    event.preventDefault ();
+
+    let Favorite1 = $("#Fav1").val()
+    let Favorite2 = $("#Fav2").val()
+    let Favorite3 = $("#Fav3").val()
+
+
 
 });
